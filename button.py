7e0095d28067a6658c5fd248451ae3b3716cfc4b
5ilv4n0 <silvano.wegener@googlemail.com>
2015-01-30 02:45:40 +0100

    changes...

--- button.py
+++ button.py
@@ -188,7 +188,10 @@ while True:
         volume_mem = volume
         button.percent_volume(volume, muted)
     else:
-        button.percent_play(33)
+        player_id = kodi.is_playing()
+        if not player_id == False:
+            percent = kodi.Player.GetProperties(playerid=player_id, properties=["percentage"])
+            button.percent_play(33)
 
 
 
